95e9781a2eb3e6978cadc012010d1f7872891390
Abdulnasirym <abdulnasiry03@gmail.com>
2023-08-19 11:04:18 +0000

    all

--- bin.h
+++ bin.h
@@ -9,29 +9,26 @@ int printf_bin(va_list val)
 {
 	int flag = 0;
 	int cont = 0;
-	int i, bit;
-
+	int i, a = 1, b;
 	unsigned int num = va_arg(val, unsigned int);
+	unsigned int p;
 
-	for (i = 31; i >= 0; i--)
+	for (i = 0; i < 32; i++)
 	{
-		bit = (num >> i) & 1;
-
-		if (bit == 1)
-		flag = 1;
-
+		p = ((a << (31 - i)) & num);
+		if (p >> (31 - i))
+			flag = 1;
 		if (flag)
 		{
-			putchar(bit + '0');
+			b = p >> (31 - i);
+			_putchar(b + 48);
 			cont++;
 		}
 	}
-
 	if (cont == 0)
 	{
-		putchar('0');
 		cont++;
+		_putchar('0');
 	}
-
-	return cont;
+	return (cont);
 }

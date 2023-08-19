b2cef9a64630f0d4a3b70871533303134d232f1e
Abdulnasirym <abdulnasiry03@gmail.com>
2023-08-19 11:20:33 +0000

    all tasks

--- printf_bin.c
+++ printf_bin.c
@@ -7,31 +7,33 @@
  */
 int printf_bin(va_list val)
 {
-	int flag = 0;
-	int cont = 0;
-	int i, bit;
+  int flag = 0;
+    int cont = 0, i, bit;
 
-	unsigned int num = va_arg(val, unsigned int);
+    unsigned int num = va_arg(val, unsigned int);
+    unsigned int mask = 1 << 31;
 
-	for (i = 31; i >= 0; i--)
-	{
-		bit = (num >> i) & 1;
+    for (i = 0; i < 32; i++)
+    {
+        bit = (num & mask) ? 1 : 0;
+        
+        if (bit == 1)
+            flag = 1;
+        
+        if (flag)
+        {
+            putchar(bit + '0');
+            cont++;
+        }
+        
+        mask >>= 1;
+    }
 
-		if (bit == 1)
-		flag = 1;
+    if (cont == 0)
+    {
+        putchar('0');
+        cont++;
+    }
 
-		if (flag)
-		{
-			putchar(bit + '0');
-			cont++;
-		}
-	}
-
-	if (cont == 0)
-	{
-		putchar('0');
-		cont++;
-	}
-
-	return (cont);
+    return (cont);
 }

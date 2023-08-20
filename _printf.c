5f59dfd5df3437c085a299babd2cc99f66ea46f6
Abdulnasirym <abdulnasiry03@gmail.com>
2023-08-20 16:24:45 +0000

    commit

--- _printf.c
+++ _printf.c
@@ -22,7 +22,7 @@ int _printf(const char * const format, ...)
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
-Place:
+Here:
 	while (format[i] != '\0')
 	{
 		j = 13;
@@ -32,7 +32,7 @@ Place:
 			{
 				len += m[j].f(args);
 				i = i + 2;
-				goto Place;
+				goto Here;
 			}
 			j--;
 		}

3cb732a87c669a25db9dda93ebe1e3050b871697
Abdulnasirym <abdulnasiry03@gmail.com>
2023-08-20 16:21:44 +0000

    commit

--- _printf.c
+++ _printf.c
@@ -25,8 +25,8 @@ int _printf(const char * const format, ...)
 Here:
 	while (format[i] != '\0')
 	{
-		j = 13;
-		while (j >= 0)
+		j = 0;
+		while (j <= 13)
 		{
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
 			{
@@ -34,7 +34,7 @@ Here:
 				i = i + 2;
 				goto Here;
 			}
-			j--;
+			j++;
 		}
 		_putchar(format[i]);
 		len++;
